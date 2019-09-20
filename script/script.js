bb13a91a86e12188e088bace02ac4758df707591
Saurabh Singh <srbsingh3@gmail.com>
2019-09-20 19:23:41 +0530

    added script

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const tableItems = {
+    "row1" : "1",
+    "row2" : "2",
+    "row3" : "3"
+}
+
+
+// $('.table').append('<tr><td>test</td></tr>')
+
+const arrayItems = ['1', '2', '3'];
+
+arrayItems.map(item => $('.table').append(`<tr><td>${item}</td></tr>`));
+
+console.log("test");
+
+
+// Use an array for the sidebar menu items.
